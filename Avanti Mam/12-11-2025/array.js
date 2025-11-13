94cfc1cbe311376a20a3043858a0b439a5acc84e
Yashodip <yahodip1602@github.com>
2025-11-13 11:43:16 +0530

    date 12 practice

---
+++ Avanti Mam/12-11-2025/array.js
@@ -0,0 +1,24 @@
+let num = [10,20,30,40,50]
+console.log(num);
+console.log(typeof(num));
+
+let arr = ["hello", "world",10];
+console.log(arr);
+console.log(typeof(arr));
+
+let fruit = ["apple","orange","banana"]
+console.log(fruit);
+console.log(typeof(fruit));
+
+let mixed = ["hi", 10,true, null]
+console.log(mixed);
+console.log(typeof(mixed));
+
+let str = [24n , "hello", true]
+console.log(str);
+console.log(typeof(str));
+
+
+
+
+
---
+++ Avanti Mam/12-11-2025/object.js
@@ -0,0 +1,24 @@
+let apple = { name: "apple", color: "red" };
+console.log(apple);
+console.log(typeof(apple));
+
+
+let banana = { name: "banana", color: "yellow" };
+console.log(banana);
+console.log(typeof(banana));
+
+
+let cherry = { name: "cherry", color: "red" };
+console.log(cherry);
+console.log(typeof(cherry));
+
+
+let carrot = { name: "carrot", color: "orange" };
+console.log(carrot);
+console.log(typeof(carrot));
+
+
+let watermelon = { name: "watermelon", color: "green" };
+console.log(watermelon);
+console.log(typeof(watermelon));
+
